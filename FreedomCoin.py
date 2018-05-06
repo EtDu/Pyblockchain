a3bdaff9362aa942a5eabe76d921bbaa969e690c
ET+ALEX CryptoWork <CryptoWork@Mais-MacBook-Pro.local>
2018-05-06 17:54:07 +0800

    Implemented proof of work

--- FreedomCoin.py
+++ FreedomCoin.py
@@ -10,12 +10,28 @@ class Block():
         self.data = data
         self.hash = self.calculate_hash()
 
+    nonce = 0
+
     def calculate_hash(self):
-        return hashlib.sha256((f"{self.index} {self.previous_hash} {self.time_stamp} {json.dumps(self.data)}").encode()).hexdigest()
+        return hashlib.sha256((f"{self.index} {self.previous_hash} {self.time_stamp} {json.dumps(self.data)} {self.nonce}").encode()).hexdigest()
+
+    def mine_block(self, difficulty):
+        zeroes = ""
+        i = 0
+        while i < difficulty:
+            zeroes += "0"
+            i += 1
+        while self.hash[:difficulty] != zeroes:
+            self.nonce += 1
+            self.hash = self.calculate_hash()
+
+
+
     
 class Blockchain():
-    def __init__(self):
+    def __init__(self, difficulty):
         self.chain = [self.create_genesis_block()]
+        self.difficulty = difficulty
     
     def create_genesis_block(self):
         return Block(0, "01/01/2018", "Genesis Block", "0")
@@ -25,7 +41,7 @@ class Blockchain():
 
     def add_block(self, new_block):
         new_block.previous_hash = self.get_latest_block().hash
-        new_block.hash = new_block.calculate_hash()
+        new_block.mine_block(self.difficulty)
         self.chain.append(new_block)
 
     def is_chain_valid(self):
@@ -43,17 +59,19 @@ class Blockchain():
 
 
 
-freedom_coin = Blockchain()
+freedom_coin = Blockchain(4)
+print("Mining block one...")
 freedom_coin.add_block(Block(1, "1,1", {"statement" : "Freedom for all!"}, ''))
+print("Mining block two...")
 freedom_coin.add_block(Block(2, "1,1", {"statement" : "Freedom for all!"}, ''))
-
-print("is the chain valid? " + str(freedom_coin.is_chain_valid()))
 
 def print_chain():
     for obj in freedom_coin.chain:
         pprint(vars(obj))
 
 print_chain()
+
+
 
 
 
